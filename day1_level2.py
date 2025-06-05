d642a82ee4a43bdd9a555713fa4805014181cc5e
UP250046 <up250046@alumnos.upa.edu.mx>
2025-06-05 10:37:37 -0600

    Distancia entre dos puntos

---
+++ day1_level2.py
@@ -0,0 +1,39 @@
+## Day 2: 30 Days of python programming
+
+## Dia 1 ---------------------------------------------------
+
+## Nivel 2
+print (3+4)
+print (3-4)
+print (3*4)
+print (3%7)
+print (3/7)
+print (3**7)
+
+print ('Deivi')
+print ('Mrq')
+print ('Mekishiko')
+print ('estoydisfrutandolostreintadiasdepython:))')
+
+print(type(10))
+print ((9.8))
+print ((3.14))
+print ((4-4j))
+print (['Asabeneh', 'Python', 'Finland'])
+
+## Nivel 3
+import math
+
+x1=   
+
+## Dia 2 ---------------------------------------------------
+FirstName = ('Deivi')
+LastName = ('Mrq')
+FullName = ('Deivi Mrq')
+Country = ('Mekishiko')
+City = ('Aguasukarientesu')
+Age = 22
+Year = 2002
+Married = ('Yeepers')
+is_true  = True
+is_light_on = ('ok')
